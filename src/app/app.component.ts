f137450308ab3062918928fbd32b0df16c552961
Luis Contreras <luisksilvacontreras@gmail.com>
2024-03-15 14:37:03 -0500

    Fin de seccion 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'bases';
-}
+  public title:string = 'Hola Mundo!';
+  public counter:number = 10;
+
+
+  setTen(): void{
+     this.counter = 10
+  }
+
+  plusOne(One:number): void{
+    this.counter += One;
+  }
+
+
+  value: string = ''
+  changeValue(){
+    this.value= 'Hola papus'
+  }
+
+
+  }
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { CounterComponent } from './counter/Components/counter/counter.components';
+import { FormsModule } from '@angular/forms';
+import { CounterModule } from './counter/Components/counter.module';
+import { ListComponent } from './Heroes/list/list.component';
+import { HeroComponent } from './Heroes/Hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    CounterModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
